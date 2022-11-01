0d293a3c3450b5e68351efd5f69455a772b5904b
AdrianPinero <102536977+AdrianPinero@users.noreply.github.com>
2022-10-31 21:43:11 -0400

    Delete Aceleracion directory

--- Aceleracion/Airflow/DAGs/dag_dinamico_grupo_univ_G.py
+++
@@ -1,9 +0,0 @@
-import dagfactory
-from airflow import DAG
-
-config_file = "/usr/local/airflow/dags/dag_dinamico_grupo_univ_G.yml"
-dag_factory = dagfactory.DagFactory(config_file)
-
-# Creating task dependencies
-dag_factory.clean_dags(globals())
-dag_factory.generate_dags(globals())
--- Aceleracion/src/py_functions/extract_fac_latam_univ_jfk.py
+++
@@ -1,9 +0,0 @@
-from extract_fac_latam import extraer_latinoam
-from extract_univ_jfk import extraer_univ_jfk
-
-
-def extrac_fac_y_univ():
-    """Ejecuta la extracción de datos de la universidad J.F. Kennedy y de la
-    Facultad Latinoamericana de Ciencias Sociales"""
-    extraer_latinoam()
-    extraer_univ_jfk()
